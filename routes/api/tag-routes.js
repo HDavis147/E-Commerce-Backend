8617e88f227a1268653cca38b1dc8ea138575868
Houston Davis <hdavis.147@gmail.com>
2023-08-31 21:02:37 -0500

    Start tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,18 +3,38 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  await Tag.findAll({
+    attributes: ['id', 'tag_name'],
+    include: [{
+      model: Product,
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+    }]
+  })
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  await Tag.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: ['id', 'tag_name'],
+    include: [{
+      model: Product,
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+    }]
+  })
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  await Tag.create({
+    tag_name: req.body.tag_name
+  })
 });
 
 router.put('/:id', (req, res) => {
